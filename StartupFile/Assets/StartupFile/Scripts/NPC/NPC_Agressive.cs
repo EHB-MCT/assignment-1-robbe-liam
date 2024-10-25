a14c1ada44f07f10c015d83dfe9c651f06e80163
RobbeDeClerck <robbe.de.clerck@student.ehb.be>
2024-10-25 15:35:57 +0200

    chore: interface implementation

---
+++ StartupFile/Assets/StartupFile/Scripts/NPC/NPC_Agressive.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC_Agressive : IWander, ITalk, IAttack, IJump
+{
+    public void Attack()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Jump()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Talk()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Wander()
+    {
+        throw new System.NotImplementedException();
+    }
+}
